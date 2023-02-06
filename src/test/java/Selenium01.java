7504dd4a3d27c697e3e191087ecd1dd08a5da8c4
Miyase Colak <colak.miyase44@gmial.com>
2023-02-06 21:42:01 +0100

    February 2023

--- src/test/java/Selenium01.java
+++ src/test/java/Selenium01.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Selenium01 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium01 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/drivers/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+    }
 }
